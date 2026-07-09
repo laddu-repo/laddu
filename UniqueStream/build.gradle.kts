4bb28e31f6419485c9d402a61d24e37de2ee7f76
laddu-repo <contact@uniquestream.net>
2026-07-10 02:31:33 +0530

    Set UniqueStream status = 1 to enable the plugin in Cloudstream

--- UniqueStream/build.gradle.kts
+++ UniqueStream/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     language = "en"
     description = "UniqueStream - Dynamic Anime Provider (with AES-256-CTR playback decryption support)."
     authors = listOf("raghav")
-    status = 0
+    status = 1
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
     iconUrl = "https://anime.uniquestream.net/favicon.ico"
     requiresResources = false
