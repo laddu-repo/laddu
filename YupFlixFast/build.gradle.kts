9e6f5c55747a605f878dd64e844b3366463f4a53
Raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-07-10 21:01:19 +0000

    add yupflixfast

---
+++ YupFlixFast/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 2
+
+cloudstream {
+    language = "hi"
+    description = "YupFlix Fast - No batching, full parallel fetch"
+    authors = listOf("raghav")
+
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    iconUrl = "https://watch.yupflix.org/favicon.svg"
+}
