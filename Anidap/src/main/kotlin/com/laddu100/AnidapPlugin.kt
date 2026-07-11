4e5b6b4b1563cb949351b207714169d2fc0a74e2
Raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-07-11 01:29:19 +0000

    fix anidap

--- Anidap/src/main/kotlin/com/laddu100/AnidapPlugin.kt
+++ Anidap/src/main/kotlin/com/laddu100/AnidapPlugin.kt
@@ -1,11 +1,12 @@
 package com.laddu100
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class AnidapPlugin : Plugin() {
-    override fun load() {
+    override fun load(context: Context) {
         // Initialize the anti-bot bypass store (loads persisted _amx_id cookies)
         initAnidapCFBypass(context)
         registerMainAPI(AnidapProvider())
