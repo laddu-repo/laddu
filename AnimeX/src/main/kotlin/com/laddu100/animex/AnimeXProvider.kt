e445168af47079259ce0e5a9866369856bc3b3d1
Raghav <raghav@users.noreply.github.com>
2026-07-27 05:10:11 +0000

    fix animex trim

--- AnimeX/src/main/kotlin/com/laddu100/animex/AnimeXProvider.kt
+++ AnimeX/src/main/kotlin/com/laddu100/animex/AnimeXProvider.kt
@@ -180,7 +180,7 @@ class AnimeXProvider : MainAPI() {
             .replace(Regex("[^a-z0-9\\s-]"), "")
             .replace(Regex("\\s+"), "-")
             .replace(Regex("-+"), "-")
-            .trim("-") + "-$id"
+            .trimStart("-").trimEnd("-") + "-$id"
     }
 }
 
